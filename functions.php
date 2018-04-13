1ae8d945d8f33e086b3697a931f9ae5c066c8d98
knorjador <headers@protonmail.com>
2018-04-13 10:34:59 +0100

    Initial Commit

---
+++ functions.php
@@ -0,0 +1,332 @@
+
+<?php
+
+/**** **** **** **** **** **** **** ****
+  > GLOBALS
+**** **** **** **** **** **** **** ****/
+
+define('MIN', 1);
+define('MAX', 10);
+
+/**** **** **** **** **** **** **** ****
+  > COMMON
+**** **** **** **** **** **** **** ****/
+
+function multiply(int $multiplier): string {
+
+  $table = '<h6>Table de '.$multiplier.'</h6>';
+
+  for($i = MIN; $i <= MAX; $i++)
+    $table .= '<p><b>'.$multiplier.' x '.$i.' = <span class="s-result">'.$i * $multiplier.'</span></b></p>';
+
+  return $table;
+
+}
+
+/**** **** **** **** **** **** **** ****
+  > RENDER
+**** **** **** **** **** **** **** ****/
+
+function render(string $exercise): string {
+
+  switch($exercise) {
+
+    case '1':
+      return renderTable();
+      break;
+
+    case '2':
+      return renderSelect();
+      break;
+
+    case '3':
+      return renderCheckboxes();
+      break;
+
+    case '4':
+     return renderSelect();
+     break;
+
+    default:
+      return false;
+      break;
+
+  }
+
+}
+
+function renderTable(): string {
+
+  return '
+
+    <div class="col s10 offset-s1 l8 offset-l2 exercise">
+      '.multiply(3).'
+    </div>
+
+  ';
+
+}
+
+function renderSelect(): string {
+
+  return '
+
+      <div class="col s10 offset-s1 l8 offset-l2 exercise">
+        <div class="d-field">
+          <div class="input-field col s6 m4 xl2">
+            '.selectMultiplier().'
+          </div>
+          <button class="btn waves-effect waves-light" type="submit">Valider</button>
+        </div>
+      </div>
+
+  ';
+
+}
+
+function renderCheckboxes(): string {
+
+  return '
+
+      <div class="col s10 offset-s1 l8 offset-l2 exercise">
+        <p class="p-choices">Choisir Tables</p>
+        <div class="input-field col s12">
+          '.checkboxesMultiplier().'
+        </div>
+        <button class="btn waves-effect waves-light btn-top" type="submit">Valider</button>
+      </div>
+
+  ';
+
+}
+
+function selectMultiplier(): string {
+
+  $select = '<select>
+              <option value="" disabled selected>Choisir Table</option>';
+
+  for($i = MIN; $i <= MAX; $i++)
+    $select .= '<option value="'.$i.'">Table de '.$i.'</option>';
+
+  return $select .= '</select>';
+
+}
+
+function checkboxesMultiplier(): string {
+
+  $checkboxes = '';
+
+  for($i = MIN; $i <= MAX; $i++) {
+
+    $checkboxes .= '
+
+      <label>
+        <input name="checkbox" value="'.$i.'" type="checkbox">
+        <span>'.$i.'</span>
+      </label>
+
+    ';
+
+  }
+
+  return $checkboxes;
+
+}
+
+/**** **** **** **** **** **** **** ****
+  > DATA
+**** **** **** **** **** **** **** ****/
+
+function getData($exercise, $data) {
+
+  switch($exercise) {
+
+    case '2':
+      return dataSelect($data);
+      break;
+
+    case '3':
+      return dataCheckboxes($data);
+      break;
+
+    case '4':
+     return dataGame($data);
+     break;
+
+    default:
+      return false;
+      break;
+
+  }
+
+}
+
+function dataSelect(string $table) {
+
+  if(intval($table)) {
+
+    return '
+
+      <div class="d-content">
+        '.multiply($table).'
+      </div>
+
+    ';
+
+  }
+
+  return false;
+
+}
+
+function dataCheckboxes(array $tables): string {
+
+  $data = '<div class="d-content">
+            <div class="d-tables">';
+
+  foreach($tables as $table)
+    $data .= '<div>'.multiply($table).'</div>';
+
+  return $data .= '</div></div>';
+
+}
+
+function dataGame($data) {
+
+  if(gettype($data) == 'string')
+    return displayGame($data);
+  else
+    return displayScore($data);
+
+}
+
+function displayGame(string $table) {
+
+  if(intval($table)) {
+
+    $numbers  = getNumbers();
+    $survey   = getQuestions($table, $numbers);
+
+    return $survey;
+
+  }
+
+  return false;
+
+}
+
+function getNumbers(): array {
+
+  $numbers  = [];
+
+   while(count($numbers) < 5 ) {
+
+     $rand            = mt_rand(1, 10);
+     $numbers[$rand]  = $rand;
+
+   }
+
+  sort($numbers);
+
+  return $numbers;
+
+}
+
+function getQuestions(string $multiplier, array $numbers) {
+
+  $survey = '<div class="d-content questions" data-table="'.$multiplier.'">';
+
+  foreach($numbers as $key => $value) {
+
+    $survey .= '<div class="col s6 m4 l2 d-question">
+                  <div class="d-field input-field">
+                    <span class="question">'.$value.' x '.$multiplier.' =</span>
+                    <input class="response" type="text" data-number="'.$value.'">
+                  </div>
+                </div>';
+
+  }
+
+  return $survey .= '<button id="score" class="btn waves-effect waves-light" type="submit">Score</button></div>';
+
+}
+
+function displayScore($data) {
+
+  $table    = $data->table;
+  $values   = $data->values;
+  $length   = count($values);
+  $score    = 0;
+  $errors   = [];
+
+  for($i = 0; $i < $length; $i++) {
+
+    if(($values[$i]->number * $table) == $values[$i]->response) {
+
+      array_push($errors, false);
+
+      $score++;
+
+    } else {
+
+      array_push($errors, true);
+
+    }
+
+  }
+
+  $score === 5 ? $sound = 'win' : $sound = 'lose';
+
+  return [
+
+      'errors'  => $errors
+    , 'score'   => '<p class="p-score">'.$score.' / '.$length.'</p>'
+    , 'sound'   => $sound
+
+  ];
+
+}
+
+/*
+if($display === 'play') {
+
+  $table = $data->table;
+
+  if(strlen($table) === 1) {
+
+    $casted = intval($table);
+
+    if($casted > 0 && $casted <= 10) {
+
+      $back = displayGame($table);
+
+    }
+
+  }
+
+}
+
+if($display === 'score') {
+
+  $table = $data->table;
+
+  if(strlen($table) === 1) {
+
+    $casted = intval($table);
+
+    if($casted > 0 && $casted <= 10) {
+
+      $values = $data->values;
+
+      $back = displayScore($table, $values);
+
+    }
+
+  }
+
+}
+
+}
+*/
+
+?>
---
+++ home.js
@@ -0,0 +1,309 @@
+
+// console.log(M)
+
+const   tabs            = document.querySelector('.tabs')
+      , instanceTabs    = M.Tabs.init(tabs)
+      , content         = document.querySelector('#content')
+
+/**** **** **** **** **** **** **** ****
+  > EVENTS
+**** **** **** **** **** **** **** ****/
+
+const eventTabs = () => {
+
+  const   elTabs  = document.querySelectorAll('[data-exercise]')
+        , length  = elTabs.length
+
+  for(let i = 0; i < length; i++)
+    elTabs[i].addEventListener('click', () => post({ display: 'render', exercise:  elTabs[i].dataset.exercise }, render))
+
+}
+
+const eventSelect = (exercise, select) => {
+
+  const button = document.querySelector('button')
+
+  button.addEventListener('click', e => {
+
+    e.preventDefault()
+
+    post({ display: 'data', exercise, data: select.value }, displayData)
+
+  })
+
+}
+
+const eventCheckboxes = () => {
+
+  const button = document.querySelector('button')
+
+  button.addEventListener('click', e => {
+
+    e.preventDefault()
+
+    const   checkedBoxes  = document.querySelectorAll('input[name=checkbox]:checked')
+          , length        = checkedBoxes.length
+          , tables        = []
+
+    for(let i = 0; i < length; i++)
+      tables.push(checkedBoxes[i].value)
+
+    post({ display: 'data', exercise: '3', data: tables }, displayData)
+
+  })
+
+}
+
+const eventScore = () => {
+
+  const score = document.querySelector('#score')
+
+  score.addEventListener('click', e => {
+
+    e.preventDefault()
+
+    const   table       = document.querySelector('.questions').dataset.table
+          , responses   = document.querySelectorAll('.response')
+          , length      = responses.length
+          , values      = []
+
+    for(let i = 0; i < length; i++) {
+
+      const response = responses[i]
+
+      values.push({ number: response.dataset.number, response: response.value })
+
+    }
+
+    post({ display: 'data', exercise: '4', data: { table, values } }, displayScore)
+
+  })
+
+}
+
+const eventRetry = () => {
+
+  const   responses   = document.querySelectorAll('.response')
+        , length      = responses.length
+
+  for(let i = 0; i < length; i++) {
+
+    const response = responses[i]
+
+    response.addEventListener('focusin', e => {
+
+      if(response.classList.contains('error'))
+        response.classList.remove('error')
+
+      if(response.classList.contains('success'))
+        response.classList.remove('success')
+
+      response.value = ''
+
+    })
+
+    response.addEventListener('focusout', e => {
+
+      const value = response.value
+
+      value.length === 0 ? response.classList.add('error') : false
+
+    })
+
+  }
+
+}
+
+/**** **** **** **** **** **** **** ****
+  > POST
+**** **** **** **** **** **** **** ****/
+
+const post = (data, cb) => { //console.log(data)
+
+  const xhr = new XMLHttpRequest()
+
+  xhr.open('post', 'ajax.php')
+
+  xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
+
+  xhr.send(`data=${JSON.stringify(data)}`)
+
+  xhr.onreadystatechange = () => { //console.log(xhr)
+
+    if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200)
+      cb(data.exercise, xhr.responseText)
+
+  }
+
+}
+
+/**** **** **** **** **** **** **** ****
+  > CALLBACKS
+**** **** **** **** **** **** **** ****/
+
+const render = (exercise, data) => { //console.log(data)
+
+  content.innerHTML = ''
+
+  switch (exercise) {
+
+    case '1':
+      renderTable(data)
+      break
+
+    case '2':
+      renderSelect(exercise, data)
+      break
+
+    case '3':
+      renderCheckboxes(data)
+      break
+
+    case '4':
+      renderSelect(exercise, data)
+      break
+
+    default: return false
+
+  }
+
+}
+
+const renderTable = data => content.insertAdjacentHTML('afterbegin', data)
+
+const renderSelect = (exercise, data) => {
+
+  content.insertAdjacentHTML('afterbegin', data)
+
+  const   select          = document.querySelector('select')
+        , instanceSelect  = M.FormSelect.init(select)
+
+  eventSelect(exercise, select)
+
+}
+
+const renderCheckboxes = data => {
+
+  content.insertAdjacentHTML('afterbegin', data)
+
+  eventCheckboxes()
+
+}
+
+const displayData = (exercise, data) => { //console.log(data)
+
+  switch (exercise) {
+
+    case '2':
+      displaySelect(data)
+      break
+
+    case '3':
+      displayCheckboxes(data)
+      break
+
+    case '4':
+      displayGame(data)
+      break
+
+    default: return false
+
+  }
+
+}
+
+const displaySelect = data => { //console.log(data)
+
+  const exercise = document.querySelector('.exercise')
+
+  if(document.querySelector('.d-content'))
+    document.querySelector('.d-content').parentNode.removeChild(document.querySelector('.d-content'))
+
+  exercise.insertAdjacentHTML('beforeend', data)
+
+}
+
+const displayCheckboxes = data => { //console.log(data)
+
+  const exercise = document.querySelector('.exercise')
+
+  if(document.querySelector('.d-content'))
+    document.querySelector('.d-content').parentNode.removeChild(document.querySelector('.d-content'))
+
+  exercise.insertAdjacentHTML('beforeend', data)
+
+}
+
+const displayGame = data => { //console.log(data)
+
+  const exercise = document.querySelector('.exercise')
+
+  if(document.querySelector('.d-content'))
+    document.querySelector('.d-content').parentNode.removeChild(document.querySelector('.d-content'))
+
+  exercise.insertAdjacentHTML('beforeend', data)
+
+  if(document.querySelector('#score'))
+    eventScore()
+
+}
+
+const displayScore = (exercise, data) => { //console.log(data)
+
+  const   back        = JSON.parse(data)
+        , errors      = back.errors
+        , score       = back.score
+        , sound       = back.sound
+        , elScore     = document.querySelector('#score')
+        , pScore      = document.querySelector('.p-score')
+        , elAudio     = document.querySelector('#audio')
+        , responses   = document.querySelectorAll('.response')
+        , length      = responses.length
+
+  pScore !== null ? pScore.parentNode.removeChild(pScore) : false
+
+  elScore.insertAdjacentHTML('afterend', score)
+
+  if(elAudio) elAudio.parentNode.removeChild(elAudio)
+
+  const audio = `<audio id="audio" src="assets/${sound}.wav" autoplay></audio>`
+
+  document.body.insertAdjacentHTML('afterbegin', audio)
+
+  if(document.querySelector('#audio')) {
+
+    const currentAudio = document.querySelector('#audio')
+
+    currentAudio.onended = () => currentAudio.parentNode.removeChild(currentAudio)
+
+  }
+
+  for(let i = 0; i < length; i++) {
+
+    if(errors[i] === true) {
+
+      responses[i].classList.add('error')
+
+    } else {
+
+      responses[i].classList.remove('error')
+      responses[i].classList.add('success')
+
+    }
+
+  }
+
+  eventRetry()
+
+}
+
+/**** **** **** **** **** **** **** ****
+  > INIT
+**** **** **** **** **** **** **** ****/
+
+const init = function() {
+
+  post({ display: 'render', exercise: '1' }, render)
+  eventTabs()
+
+}()
