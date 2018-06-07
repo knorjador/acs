21c9cda76e81120696ecf79be9ecde8e81bec1fa
wawrzyniakarnaud <arnaud.wawrzyniak@gmail.com>
2018-06-07 13:53:28 +0200

    Add Listing

--- models/ListingModel.php
+++ models/ListingModel.php
@@ -1,5 +1,10 @@
-
 <?php
 
-
+function getMemes($pdo) {
+     $memes = $pdo->getInstance()->prepare("SELECT * FROM `memes` ORDER BY date DESC");
+     $memes->execute();
+     $data = $memes->fetchAll();
+     return $data;
+}
 ?>
+
