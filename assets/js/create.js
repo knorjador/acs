11f79d61d6b974e95e5053f068bbe61e4db4f73c
PierreHermey <pierre.hermey@laposte.net>
2018-06-12 09:41:43 +0200

    dropdown for adding API Giphy

--- assets/js/create.js
+++ assets/js/create.js
@@ -275,4 +275,7 @@ const init = function() {
 
 }()
 
-
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.dropdown-trigger');
+  var instances = M.Dropdown.init(elems);
+});
