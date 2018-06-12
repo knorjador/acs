808bb451904b32314fd370d96a9f3936b0caa3d9
knorjador <headers@protonmail.com>
2018-06-12 13:44:22 +0100

    add gif routing

--- assets/js/listing.js
+++ assets/js/listing.js
@@ -1,9 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);
-  });
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.dropdown-trigger');
-    var instances = M.Dropdown.init(elems);
-  });
+document.addEventListener('DOMContentLoaded', () => {
+
+  const   sideNav      = document.querySelectorAll('.sidenav')
+        , initSideNav  = M.Sidenav.init(sideNav)
+        , dropdown     = document.querySelectorAll('.dropdown-trigger')
+        , initDropdown = M.Dropdown.init(dropdown)
+
+})
--- models/CreateModel.php
+++ models/CreateModel.php
@@ -1,17 +1,17 @@
 
 <?php
 
-function getBases($pdo): array {
-    
-    $statement = $pdo->getInstance()->prepare("SELECT * FROM bases");
+function getImages($pdo): array {
+
+    $statement = $pdo->getInstance()->prepare("SELECT * FROM images");
     $statement->execute();
-    
+
     $data = $statement->fetchAll();
-    
+
     // echo '<pre>';
     // var_dump($data);
     // echo '</pre>';
-    
+
     return $data;
 }
 
